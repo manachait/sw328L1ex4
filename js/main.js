a5588de7ebdaa98716148945ba360d2e18db98ee
manachait <manachai_toa@utcc.ac.th>
2016-01-18 15:21:13 +0700

    Format function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function calculate(){
     var cel = document.getElementById("celcius");
-	var far = ((cel.value * 9)/5) + 32;
-	var kel = parseFloat(cel.value) + 273.15;
-	document.getElementById("farenheit").innerHTML = "It equals " + far + " Farenheit";
-	document.getElementById("kelvin").innerHTML = "It equals " + kel + " Kelvin";
+    var far = ((cel.value * 9)/5) + 32;
+    var kel = parseFloat(cel.value) + 273.15;
+    document.getElementById("farenheit").innerHTML = "It equals " + far + " Farenheit";
+    document.getElementById("kelvin").innerHTML = "It equals " + kel + " Kelvin";
 }
